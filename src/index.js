75daa0bca407e4ef6c397e055d9498ee0d85b71a
Mahopeok <hopemaha11@gmail.com>
2025-05-16 03:07:37 +0100

    Added axios, AI Api integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
 
-    
-   
 new Typewriter('#poem', {
-  strings: " The flowers bloom in the spring, <br> The birds begin to sing, <br> The sun shines bright and warm, <br> Nature awakens from its dorm. <br> The trees are green and full of life, <br> A time for joy, a time for strife. <br> The world is fresh and new again, <br> A season of hope, a season of zen. <br>",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor:""
 });
+
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "bd13391t3d87a406o7b70e7f7fa17f8e";
+  let prompt = `User instruction: Generate a french poem about ${userInput.value}`;    
+  let context = "You are a romantic poem expert and love to write short poems. Your mision is to generate a 4 line poem in basic format and separate each line with a <br/>. Make sure to follow the user instructions and sign at the end of the poem with 'Shecodes AI' in <strong> tag."; 
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayPoem);
+
+
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem); 
